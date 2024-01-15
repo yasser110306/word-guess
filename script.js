fb8344e7e68fb93dfefe046700021b20e40c2995
yasser110306 <yasserrabie110306@gmail.com>
2024-01-15 17:43:14 +0200

    feats added
    1- (win - lose) handler.
    
    2- win and lose notification.

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
 const answer = "hamdy";
 const answerArr = answer.split("");
-const charchterBox = document.querySelectorAll(".try1 input");
+let numberOfTry = 1;
+let charchterBox = document.querySelectorAll(
+  `[data-tryNumber = "${numberOfTry}"] input`
+);
 
 // limiting N of chars
-const inputLimit = 1;
 charchterBox.forEach((box) => {
   box.maxLength = "1";
 });
 
+function tryChecker() {
+  if (numberOfTry <= 5) {
+    checkBoxes();
+  }
+}
+// counter to clac number of right chars
+let inPlaceCounter = 0;
+
 function checkBoxes() {
+  let charBoxNumber = 0;
   // repeating the function until all chars are checked
-  for (let i = 0; i < charchterBox.length; i++) {
+  charchterBox.forEach((box) => {
     // checks if char exists
-    if (answerArr.includes(charchterBox[i].value)) {
+    if (answerArr.includes(box.value)) {
       // checks if char in place
-      if (answerArr.indexOf(charchterBox[i].value) + 1 === 1) {
+      if (answerArr.indexOf(box.value) === charBoxNumber) {
         // giving the box the in place style
-        charchterBox[i].classList.add("in-place-style");
-        charchterBox[i].style.border = "none";
+        box.classList.add("in-place-style");
+        box.style.border = "none";
 
         // making the try unchangable
-        charchterBox[i].disabled = true;
+        box.disabled = true;
+
+        inPlaceCounter++;
       } else {
         // giving the box the not in place style
-        charchterBox[i].classList.add("not-in-place-style");
-        charchterBox[i].style.border = "none";
+        box.classList.add("not-in-place-style");
+        box.style.border = "none";
 
         // making the try unchangable
-        charchterBox[i].disabled = true;
+        box.disabled = true;
       }
     } else {
       // giving the box wrong style
-      charchterBox[i].classList.add("wrong-style");
-      charchterBox[i].style.border = "none";
+      box.classList.add("wrong-style");
+      box.style.border = "none";
 
       // making the try unchangable
-      charchterBox[i].disabled = true;
+      box.disabled = true;
     }
+
+    // goes to next input
+    charBoxNumber++;
+  });
+
+  // judge after try is checked if player won or lost
+  loseOrWin();
+
+  // goes to next try
+  numberOfTry++;
+  console.log(numberOfTry);
+
+  // updating the selector to go to the next try
+  charchterBox = document.querySelectorAll(
+    `[data-tryNumber = "${numberOfTry}"] input`
+  );
+}
+
+function loseOrWin() {
+  if (inPlaceCounter === answer.length) {
+    document.querySelector(".win").style.display = "flex";
+  } else if (inPlaceCounter !== answer.length && numberOfTry === 5) {
+    document.querySelector(".lose").style.display = "flex";
   }
 }
 
 const checkWordBtn = document.getElementById("check-word-btn");
 
-checkWordBtn.addEventListener("click", checkBoxes);
+checkWordBtn.addEventListener("click", tryChecker);
